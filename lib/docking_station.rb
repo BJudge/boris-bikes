bfce5934ed81998e23acea3598d3b109d79f0318
Joe Sweeny <joesweeny84@hotmail.com>
2016-07-08 13:25:47 +0100

    Step.15 refactored

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,17 +3,27 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bikes
   def initialize
-  @bikes = []
+    @bikes = []
   end
 
   def release_bike
-    fail "No bikes!" if @bikes.empty?
-    @bikes.pop
+    fail "No bikes!" if empty?
+      @bikes.pop
   end
 
   def dock(bike)
-    fail "Cannot accept bike - Full" if @bikes.count >=20
+    fail "Cannot accept bike - Full" if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.count >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
